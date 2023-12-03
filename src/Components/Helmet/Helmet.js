471e7d791a2278e11ff63feba554e69ffc199785
Nikolaj <niko.sage@gmail.com>
2023-12-03 09:46:17 -0800

    fixed cannonical issue with param links

--- src/Components/Helmet/Helmet.js
+++ src/Components/Helmet/Helmet.js
@@ -8,21 +8,21 @@ export const MetaTags = ({data = {}, image}) => {
 
     let title = "XQuicky - The Ultimate Destination for TikTok Style Porn Videos";
 
-    let description = "Indulge in the hottest TikTok style porn videos featuring young and beautiful models. XQuicky is your go-to destination for high-quality, explicit content that will leave you breathless. Experience the thrill of TikTok style porn like never before!";
+    let description = "Indulge in the hottest TikTok style porn videos featuring beautiful models. XQuicky is your go-to destination for high-quality, explicit content that will leave you breathless. Experience the thrill of TikTok style porn like never before!";
 
 
     return (
     <Helmet>
         <meta charSet='utf-8' />
         <title>{data?.title ? `XQuicky: ${data.title}` : 'XQuicky'}</title>
-        <link rel='canonical' href={window.location.href} />
+        <link rel='canonical' href={window.location.href.split('?')[0]} />
         <meta property='og:image' name='og:image' content={image || img} />
         <meta name='og:title' content={data?.title ? `XQuicky: ${data.title}` : title} />
         <meta property='twitter:image' name='twitter:image' content={image || img} />
         <meta name='twiiter:title' content={data?.title ? `XQuicky: ${data.title}` : title} />
-        <meta name="description" content={data?.title || description} />
-        <meta name="og:description" content={data?.title || description} />
-        <meta name="twitter:description" content={data?.title || description} />
+        <meta name="description" content={description} />
+        <meta name="og:description" content={description} />
+        <meta name="twitter:description" content={description} />
     </Helmet> 
     )
 }
--- src/Util/Hints.js
+++ src/Util/Hints.js
@@ -1,4 +1,5 @@
 export const Hints = [
     "You can navigate media using the arrow keys!",
-    "For a mobile app like experience add xquicky to your home screen!"
+    "For a mobile app like experience add xquicky to your home screen!",
+    "Tap on the left or right side of galleries for traversing"
 ]
